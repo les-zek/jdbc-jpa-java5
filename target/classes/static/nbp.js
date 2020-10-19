babc3e6ee00c3870205b283b87fa1c65bacd64d0
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-10-19 12:34:50 +0200

    beginnings of HTML+ CSS + JavaScript + AJAX + fetch Promises quiz App modified 3

--- target/classes/static/nbp.js
+++ target/classes/static/nbp.js
@@ -17,14 +17,21 @@ function getPosts() {
 }
 
 function getRates() {
+
     fetch("http://api.nbp.pl/api/exchangerates/tables/A?format=json")
         .then(response => response.json())
         .then(response => {
             let div = document.getElementById("rates");
-            div.innerHTML = "";
+//            div.innerHTML = "";
+            div.innerHTML += `<h3>Tabela kursów walut</h3> <h5>${response[0].no} &nbsp&nbsp Data: ${response[0].effectiveDate}</h5><hr>`;
+            div.innerHTML += '<h5>[KOD] N A Z W A&nbsp&nbsp&nbsp&nbsp&nbspKurs średni w zł</h5>'
+
             for (rate of response[0].rates) {
-                div.innerHTML += `<p>${rate.currency}: ${rate.mid}</p>`;
+                div.innerHTML += `<p>[${rate.code}] ${rate.currency} = ${rate.mid} </p>`;
+
+
             }
+            div.innerHTML += `<h5>- KONIEC TABELI -</h5>`;
         })
         .catch(a => console.log("Odrzucono połączenie: " + a));
 }
