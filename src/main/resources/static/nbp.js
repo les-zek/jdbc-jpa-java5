2f1006d31c969aa1496615377adb25aa10a25520
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-10-19 11:15:59 +0200

    beginnings of HTML+ CSS + JavaScript + AJAX + fetch Promises quiz App modified 1

--- src/main/resources/static/nbp.js
+++ src/main/resources/static/nbp.js
@@ -1,35 +1,30 @@
 function getPosts() {
     console.log("start");
-    const ajax = new XMLHttpRequest()
+    const ajax = new XMLHttpRequest();
     ajax.open("GET", "https://jsonplaceholder.typicode.com/posts");
-
-    //   ajax.open("GET", "http://api.nbp.pl/api/exchangerates/tables/A?format=json");
     ajax.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             console.log("dane odebrane");
             const posts = JSON.parse(this.response);
             let div = document.getElementById("rates");
+            div.innerHTML = "";
             for (post of posts) {
-                //        div.innerHTML += "<p>" + post.title + "</p>";
-                div.innerHTML += `<p><strong>${post.title}</strong>:<br> ${post.body}</p>`;
-
+                div.innerHTML += `<p><strong>${post.title}</strong>:<br>${post.body}</p>`;
             }
-
         }
     }
     ajax.send();
-
 }
-// metoda Promises - fetch
-function getRates(){
+
+function getRates() {
     fetch("http://api.nbp.pl/api/exchangerates/tables/A?format=json")
-        .then(response =>response.json())
+        .then(response => response.json())
         .then(response => {
             let div = document.getElementById("rates");
-            div.innerHTML="";
-            for (rate of response[0].rates){
+            div.innerHTML = "";
+            for (rate of response[0].rates) {
                 div.innerHTML += `<p>${rate.currency}: ${rate.mid}</p>`;
-
             }
-        });
+        })
+        .catch(a => console.log("Odrzucono połączenie: " + a));
 }
